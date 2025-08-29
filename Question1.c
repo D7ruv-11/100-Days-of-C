0e63dd9865a7dd82cf4c88f54181131dbb656ea1
Dhruv Chonde <official.dhruv.nextgen@gmail.com>
2025-08-29 23:31:08 +0530

    Add files via upload

---
+++ Question1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    float number1,number2;
+    printf("enter your number one:",number1);
+    scanf("%f",&number1);
+    printf("enter your number2:", number2);
+    scanf("%f",&number2);
+    printf("Sum of hte number is:%f",number1+number2);
+    return 0;
+}
