44295a47345e47bc0bc4c95a5d9e510c9ddd9d3f
Dhruv Chonde <official.dhruv.nextgen@gmail.com>
2025-08-30 22:29:18 +0530

    Add files via upload

---
+++ Question2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+int a,b,sum,diff,product,mod,div;    
+printf("Input the  number a:",a);
+scanf("%d",&a);
+printf("Input the number b:",b);\
+scanf("%d",&b);
+sum=a+b;
+diff=a-b;
+product=a*b;
+mod=a%b;
+div=a/b;
+printf("Your sum is:%d\n",sum);
+printf("Your diff is:%d\n",diff);
+printf("Your product is:%d\n",product);
+printf("Your mod is:%d\n",mod);
+printf("Your div is:%d\n", div);
+return 0;
+
+}
