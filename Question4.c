7a955e6b25eaf772f87b90f534719be43f304a82
Dhruv Chonde <official.dhruv.nextgen@gmail.com>
2025-09-04 09:38:39 +0530

    Add files via upload

---
+++ Question4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    float circumference,area,radius;
+    printf("Enter the value of radius=",radius);
+    scanf("%f",&radius);
+    circumference= 2*3.14*radius;
+    area=3.14*radius*radius;
+    printf("Circumference is :%f\n",circumference);
+    printf("area is:%f\n", area);
+    return 0;
+
+}
