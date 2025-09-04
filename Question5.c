18e50c8f5350d5d3043172c8ba9937425e9d4ee1
Dhruv Chonde <official.dhruv.nextgen@gmail.com>
2025-09-04 09:39:08 +0530

    Add files via upload

---
+++ Question5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    float temp,fehrenheit;
+    printf("Enter the value of temprature in celcius:",temp);
+    scanf("%f",&temp);
+    fehrenheit=9.0/5.0*temp+32;
+    printf("Your temprature in  fehrenhiet:%f",fehrenheit);
+    return 0;
+
+
+    
+}
