bc7cc4d1f819b38ecf2c83fdac072ed633b5d01a
Dhruv Chonde <official.dhruv.nextgen@gmail.com>
2025-09-07 23:06:07 +0530

    Add files via upload

---
+++ Q8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+    int n ; 
+printf("input the n natural number:",n);
+ scanf("%d", &n);
+n=n*(n+1)/2;
+
+if (n>-1)
+printf("%d",n);
+else 
+printf("enter positive number! ");
+  return 0;  
+}
+
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+
