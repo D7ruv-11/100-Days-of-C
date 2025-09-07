fa1d4005b0c140aac46a6e993a1d58ccbaa7ad2a
Dhruv Chonde <official.dhruv.nextgen@gmail.com>
2025-09-08 00:17:13 +0530

    Add files via upload

---
+++ Question12.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter the number you wanna check:");
+    scanf("%d",&n);
+    if (n>0)
+        if(n==0)
+        printf("the numbet is zero ");
+        else 
+        printf("yah it is positive!");
+    else 
+    printf("the number is negative");
+    return 0;
+    
+}
